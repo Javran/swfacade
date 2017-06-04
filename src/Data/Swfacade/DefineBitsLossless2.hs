710190b32cf0194414fc648c73102b1c1d4204ad
Javran Cheng <Javran.c@gmail.com>
2017-06-03 22:19:43 -0400

    impl getAlphaColormapData

--- src/Data/Swfacade/DefineBitsLossless2.hs
+++ src/Data/Swfacade/DefineBitsLossless2.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Data.Swfacade.DefineBitsLossless2 where
 
 import Vision.Image
@@ -7,6 +8,8 @@ import Control.Monad
 import qualified Codec.Compression.Zlib as Zlib
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.Ix as Ix
+import qualified Data.Vector as V
+import Data.Word
 
 data DefineBitsLossless2 = DefineBitsLossless2
   { characterId :: Int
@@ -22,8 +25,26 @@ getData = do
     w <- fromIntegral <$> getWord16le
     h <- fromIntegral <$> getWord16le
 
-    let getAlphaColormapData :: Int -> Get RGBA
-        getAlphaColormapData ctSize = undefined
+    let sp = ix2 h w
+        getAlphaColormapData :: Int -> Get RGBA
+        getAlphaColormapData ctSize = do
+            colorTable <- V.fromList <$> replicateM (ctSize+1) getRGBA
+            let extraW = if w `mod` 4 == 0 then 0 else 4 - w `mod` 4
+                paddedW = w + extraW
+                getImgLine = do
+                    (xs,zeros) <- splitAt w <$> replicateM paddedW getWord8
+                    when (any (/= 0) zeros) $ fail "expecting paddings to be zeros"
+                    pure (V.fromList xs)
+            (imgLines :: V.Vector (V.Vector Word8)) <- V.fromList <$> replicateM h getImgLine
+            let getColor :: Point -> RGBAPixel
+                getColor (Z :. y :. x) = colorTable V.! fromIntegral ind
+                  where
+                    ind :: Word8
+                    ind = (imgLines V.! y) V.! x
+            pure (fromFunction sp getColor)
+          where
+            getRGBA :: Get RGBAPixel
+            getRGBA = RGBAPixel <$> getWord8 <*> getWord8 <*> getWord8 <*> getWord8
 
         getAlphaBitmapData :: Get RGBA
         getAlphaBitmapData = do
@@ -31,13 +52,12 @@ getData = do
             let decompressed = Zlib.decompress compressed
                 expectedLen = h * w * 4
                 actualLen = LBS.length decompressed
-                sp = ix2 h w
                 getPixelInd = Ix.index ((0,0),(h-1,w-1))
                 getColor :: Point -> RGBAPixel
                 getColor (Z :. y :. x) = RGBAPixel r g b a
                   where
                     pInd = getPixelInd (y,x)
-                    [r,g,b,a] =
+                    [a,r,g,b] =
                         (\colorInd ->
                          let ind = fromIntegral (colorInd + pInd*4)
                          in LBS.index decompressed ind) <$> [0..]
